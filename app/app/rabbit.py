5d0224eb9e7dcd6dac4ad44356ade581e9eb32f0
Михаил Богачев <bogachevmikhailirkutsk@gmail.com>
2026-03-24 17:25:33 +0800

    refactor

--- app/app/rabbit.py
+++ app/app/rabbit.py
@@ -4,25 +4,42 @@ import asyncio
 
 RABBITMQ_URL = "amqp://user:password@rabbitmq:5672/"
 EXCANGE_NAME = "price_excange"
+QUEUE_NAME = "price_updates"
+ROUTING_KEY = "price.update"
 
 
 class RabbitMq:
-    connection = None
-    channel = None
-    excange = None
+    connection: aio_pika.RobustConnection | None = None
+    channel: aio_pika.RobustChannel | None = None
+    exchange: aio_pika.Exchange | None = None
 
     @classmethod
     async def connect(cls):
         cls.connection = await aio_pika.connect_robust(RABBITMQ_URL)
         cls.channel = await cls.connection.channel()
-        cls.excange = await cls.channel.declare_exchange(EXCANGE_NAME,
-                                                        aio_pika.ExchangeType.DIRECT)
+        cls.exchange = await cls.channel.declare_exchange(
+            EXCANGE_NAME,
+            aio_pika.ExchangeType.DIRECT,
+            durable=True
+        )
+
+        queue = await cls.channel.declare_queue(
+            QUEUE_NAME,
+            durable=True,
+        )
+        await queue.bind(cls.exchange, routing_key=ROUTING_KEY)
 
     @classmethod
     async def publish_price_update(cls, message: str):
-        if cls.excange is None:
+        if cls.exchange is None:
             await cls.connect()
-        await cls.excange.publish(
+
+        await cls.exchange.publish(
             aio_pika.Message(body=message.encode()),
             routing_key="price.update"
         )
+
+    @classmethod
+    async def close(cls) -> None:
+        if cls.connection and not cls.connection.is_closed:
+            await cls.connection.close()
